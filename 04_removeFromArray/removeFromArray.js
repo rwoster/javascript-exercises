c13c022f68dca20e9f303fb0c1c22e847d7515dc
Randy Woster <rwoster@protonmail.com>
2022-08-31 16:52:34 -0400

    Refactored

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,28 @@
-const removeFromArray = (arr, num, num2, num3, num4) => {
-    num2 = num2 || null;
-    num3 = num3 || null;
-    num4 = num4 || null;
+// const removeFromArray = (arr, num, num2, num3, num4) => {
+//     num2 = num2 || null;
+//     num3 = num3 || null;
+//     num4 = num4 || null;
 
-    const filter = (arr, n) => arr.filter((i) => i !== n);
+//     const filter = (arr, n) => arr.filter((i) => i !== n);
 
-    let answer = filter(arr, num);
+//     let answer = filter(arr, num);
 
-    if (num2 != null) {
-        answer = filter(answer, num2);
-    }
-    if (num3 != null) {
-        answer = filter(answer, num3);
-    }
-    if (num4 != null) {
-        answer = filter(answer, num4);
-    }
+//     if (num2 != null) {
+//         answer = filter(answer, num2);
+//     }
+//     if (num3 != null) {
+//         answer = filter(answer, num3);
+//     }
+//     if (num4 != null) {
+//         answer = filter(answer, num4);
+//     }
 
-    return answer;
+//     return answer;
+// };
+
+const removeFromArray = (...args) => {
+    const array = args[0];
+    return array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
