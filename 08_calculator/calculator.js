b0fae0fc81c80aa8299b0b2265d910111ad9dce8
Randy Woster <rwoster@protonmail.com>
2022-09-06 17:05:42 -0400

    passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = (a, b) => a - b;
+
+const sum = function (arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const sum = function() {
-	
-};
+const multiply = (arr) =>
+    arr.length ? arr.reduce((acc, num) => acc * num) : 0;
 
-const multiply = function() {
+const power = (a, b) => a ** b;
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (a) {
+    if (a === 0) return 1;
+    let sum = 1;
+    for (let i = a; i > 0; i--) {
+        sum *= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
