22d280a39716e83a9e78dc06d68f968deffd48ac
Randy Woster <rwoster@protonmail.com>
2022-09-07 10:22:53 -0400

    passed 1st test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const rev = word.split("").reverse().join("");
 
+    if (word === rev) {
+        console.log(`word: ${word} === rev: ${rev}`);
+        return true;
+    } else {
+        console.log(`str: ${word} != rev: ${rev}`);
+        return false;
+    }
 };
+
+console.log(palindromes("racecar"));
+console.log(palindromes("randy"));
 
 // Do not edit below this line
 module.exports = palindromes;
