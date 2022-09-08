de32d698b6cf51d68162e6052986ed8575497510
Randy Woster <rwoster@protonmail.com>
2022-09-08 09:52:42 -0400

    update with Odin solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,30 @@
-const findTheOldest = function (people) {
-    const sorted = people.sort((a, b) => {
-        const notDead = new Date().getFullYear();
+const findTheOldest = function (array) {
+    // ! MY SOLUTION
+    // const sorted = array.sort((a, b) => {
+    //     const notDead = new Date().getFullYear();
+    //     const aAge = (a.yearOfDeath ? a.yearOfDeath : notDead) - a.yearOfBirth;
+    //     const bAge = (b.yearOfDeath ? b.yearOfDeath : notDead) - b.yearOfBirth;
+    //     return aAge > bAge ? -1 : 1;
+    // });
+    // return sorted[0];
 
-        const aAge = (a.yearOfDeath ? a.yearOfDeath : notDead) - a.yearOfBirth;
-        const bAge = (b.yearOfDeath ? b.yearOfDeath : notDead) - b.yearOfBirth;
-
-        return aAge > bAge ? -1 : 1;
+    // ! ODIN SOLUTION
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
     });
-    return sorted[0];
 };
 
-// const notDead = new Date().getFullYear();
-// console.log(notDead);
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
