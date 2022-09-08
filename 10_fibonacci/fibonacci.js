13fb2480c57b46b4dd3e4717249f8424bc1c5ecf
Randy Woster <rwoster@protonmail.com>
2022-09-08 08:56:46 -0400

    passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        if (num === 0) return 0;
+        let a = 0;
+        let b = 1;
+        while (num > 0) {
+            const temp = b;
+            b = a + b;
+            a = temp;
+            num--;
+        }
+        return a;
+    }
 };
+
+// fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
