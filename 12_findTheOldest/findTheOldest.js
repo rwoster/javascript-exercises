ff44c1fbfbe8bcaaed96eb11ba01476432edd2bc
Randy Woster <rwoster@protonmail.com>
2022-09-08 09:21:52 -0400

    passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const sorted = people.sort((a, b) => {
+        const notDead = new Date().getFullYear();
 
+        const aAge = (a.yearOfDeath ? a.yearOfDeath : notDead) - a.yearOfBirth;
+        const bAge = (b.yearOfDeath ? b.yearOfDeath : notDead) - b.yearOfBirth;
+
+        return aAge > bAge ? -1 : 1;
+    });
+    return sorted[0];
 };
+
+// const notDead = new Date().getFullYear();
+// console.log(notDead);
 
 // Do not edit below this line
 module.exports = findTheOldest;
